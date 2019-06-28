b184a119849625569528d5c45373cb88b7e5f85d
toastmalone <josh.m.gauntt@gmail.com>
2019-06-27 21:37:09 -0700

    loaded in data

--- SentimentAnalysis/Program.cs
+++ SentimentAnalysis/Program.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms.Text;
+
 
 namespace SentimentAnalysis
 {
+    
     class Program
     {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
